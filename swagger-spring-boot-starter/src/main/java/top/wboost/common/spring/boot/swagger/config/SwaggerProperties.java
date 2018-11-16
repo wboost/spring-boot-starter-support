0dce9f453f83279c51bf5845d2aeadff84d58648
sunjw <jwsun@sunjwdeMacBook-Pro.local>
2018-11-16 22:19:13 +0800

    swagger-ui原生离线文档导出功能

--- swagger-spring-boot-starter/src/main/java/top/wboost/common/spring/boot/swagger/config/SwaggerProperties.java
+++ swagger-spring-boot-starter/src/main/java/top/wboost/common/spring/boot/swagger/config/SwaggerProperties.java
@@ -1,8 +1,7 @@
 package top.wboost.common.spring.boot.swagger.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import top.wboost.base.spring.boot.starter.GlobalForSpringBootStarter;
 
 @Data
@@ -15,5 +14,13 @@ public class SwaggerProperties {
     private String version;
     /**默认使用@ApiParam注解生成的参数类型 {@code path}, {@code query}, {@code body}, {@code header} or {@code form}.**/
     private String defaultApiParamType;
+    /**
+     * 是否允许导出api文档
+     **/
+    private boolean enableExportApi;
+    /**
+     * 导出请求路径
+     */
+    private String exportUrl;
 
 }
