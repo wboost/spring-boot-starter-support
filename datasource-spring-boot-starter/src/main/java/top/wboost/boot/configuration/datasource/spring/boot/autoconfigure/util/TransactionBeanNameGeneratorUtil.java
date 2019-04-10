a1eea395491b250cefff6b7d0463f781b048d756
sunjw <jwsun@sunjwdeMacBook-Pro.local>
2019-04-10 18:34:52 +0800

    split

--- datasource-spring-boot-starter/src/main/java/top/wboost/boot/configuration/datasource/spring/boot/autoconfigure/util/TransactionBeanNameGeneratorUtil.java
+++
@@ -1,8 +0,0 @@
-package top.wboost.boot.configuration.datasource.spring.boot.autoconfigure.util;
-
-public class TransactionBeanNameGeneratorUtil {
-
-    public static String generatorTransactionManagerName(String dataSourceName) {
-        return dataSourceName + "_TransactionManager";
-    }
-}
